8a709c9fdee23b1ac466d2a11fafb167a895a276
fambelic <fabiolamans98@gmail.com>
2023-01-19 17:40:34 +0100

    inserito crossorigin

--- adservice/src/main/java/com/adservice/controller/AdController.java
+++ adservice/src/main/java/com/adservice/controller/AdController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 @ComponentScan("com.adservice.service")
+@CrossOrigin(origins = "http://localhost:8000")
 @RestController
 public class AdController {
     @Autowired
